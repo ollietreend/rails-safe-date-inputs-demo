caf697868fa0f45fceb7d00c0ac8039421085c29
Ollie Treend <ollie@treend.uk>
2023-08-30 09:40:40 +0100

    Add govuk_publishing_components
    To demonstrate usage of the date input component.
    
    https://components.publishing.service.gov.uk/component-guide/date_input

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
 end
