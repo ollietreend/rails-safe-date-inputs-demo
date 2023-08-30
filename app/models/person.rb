6f8fe906035825b7674ebaec015f078823448b45
Ollie Treend <ollie@treend.uk>
2023-08-30 09:44:27 +0100

    Implement safety check for date of birth field
    So it can accept bad user input (e.g. invalid dates, or partial dates)
    without raising an unhandled exception.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,27 @@
 class Person < ApplicationRecord
+  validates :name, :date_of_birth, presence: true
+  validate :date_of_birth, :valid_date_of_birth
+
+  def date_of_birth=(date)
+    if date.is_a?(Hash)
+      @date_field_validity = {} if @date_field_validity.nil?
+
+      begin
+        raise ArgumentError if date.values.any?(&:nil?)
+        Date.new(date[1], date[2], date[3])
+        @date_field_validity[:date_of_birth] = true
+      rescue ArgumentError
+        @date_field_validity[:date_of_birth] = false
+        date = nil
+      end
+    end
+
+    super(date)
+  end
+
+  def valid_date_of_birth
+    if @date_field_validity.present? && @date_field_validity[:date_of_birth] == false
+      errors.add(:date_of_birth, "must be a valid date")
+    end
+  end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def build_person(attributes = {})
+    default_attributes = { name: "John" }
+    Person.new(default_attributes.merge(attributes))
+  end
+
+  test "valid multi-parameter form input" do
+    form_input = { 1 => 2023, 2 => 8, 3 => 30 }
+    person = build_person(date_of_birth: form_input)
+    assert person.valid?
+    assert_equal Date.parse("30/08/2023"), person.date_of_birth
+  end
+
+  test "invalid multi-parameter form input" do
+    form_input = { 1 => 2023, 2 => 8, 3 => 40 }
+    person = build_person(date_of_birth: form_input)
+    assert person.invalid?
+    assert_nil person.date_of_birth
+    assert_equal ["Date of birth can't be blank", "Date of birth must be a valid date"], person.errors.to_a
+  end
+
+  test "partial multi-parameter form input" do
+    form_input = { 1 => 2023, 2 => nil, 3 => 30 }
+    person = build_person(date_of_birth: form_input)
+    assert person.invalid?
+    assert_nil person.date_of_birth
+    assert_equal ["Date of birth can't be blank", "Date of birth must be a valid date"], person.errors.to_a
+  end
+
+  test "blank date of birth" do
+    person = build_person(date_of_birth: nil)
+    assert person.invalid?
+    assert_nil person.date_of_birth
+    assert_equal ["Date of birth can't be blank"], person.errors.to_a
+  end
 end
